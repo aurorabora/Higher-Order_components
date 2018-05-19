ac4a5baab5368e2b181d443da18484eb125cdfb2
aurorabora <borabora.choi@gmail.com>
2018-05-18 17:17:31 -0700

    rendered user auth link

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,10 @@
 
 import types from './types';
+
+export function changeAuth(auth){
+    return{
+        type: types.CHANGE_AUTH,
+        auth: auth
+
+    }
+}
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,26 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { changeAuth } from '../actions';
 
 
-class Nav extends Component{
-    render(){
+class Nav extends Component {
+    renderLinks() {
+        const { auth, changeAuth } = this.props;
+        const btnStyle = {
+            width: '131px'
+        };
+
+        if (auth) {
+            return (
+                <Fragment>
+                    <li>
+                        <Link to="/secret-doc">Secret Doc</Link>
+                    </li>
+                    <li>
+                        <Link to="/operative-list"> Operative List</Link>
+                    </li>
+                    <li>
+                        <button style ={btnStyle} className="btn red darken-2" onClick={() => changeAuth(false)}>Sign Out</button>
+                    </li>
+                </Fragment>
+            )
+        }
+
+
+        return(
+            <li>
+            <button style={btnStyle} className="btn grey" onClick={() => changeAuth(true)}>Sign In</button>
+            </li>
+        );
+    }
+
+    render() {
         return (
-            <nav>
+            <nav style={{ padding: '0 12px' }}>
                 <div className="nav-wrapper">
                     <Link to="/" className="brand-logo"> CIA DATA </Link>
                     <ul className="right">
                         <li>
-                            <Link to="/"> Home</Link>
+                            <Link to="/"> Home </Link>
                         </li>
                         <li>
-                            <Link to="/about"> About</Link>
+                            <Link to="/about"> About </Link>
                         </li>
-                        <li>
-                            <Link to="/secret-doc">Secret Doc</Link>
-                        </li>
-                        <li>
-                            <Link to="/operative-list"> Operative List</Link>
-                        </li>
+
+                            {this.renderLinks()}                       
                     </ul>
                 </div>
             </nav>
@@ -28,4 +56,12 @@ class Nav extends Component{
     }
 }
 
-export default Nav;
+function mapStateToProps(state) {
+    return {
+        auth: state.user.auth
+    }
+}
+
+
+
+export default connect(mapStateToProps, { changeAuth: changeAuth })(Nav);
